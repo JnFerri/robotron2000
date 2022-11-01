07b00d3034d715480ed785e9f43e8f7b250b1d3b
Jean Ferri <jeanferrimh@gmail.com>
2022-11-01 10:32:48 -0300

    estatisticas

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import {pecas} from '/estatisticas.js'
 //alterar cor 
 
 var localImg = document.querySelector('.robotron')
@@ -9,7 +10,7 @@ for(var i=0; i <=quantidadeDeCor; i++){
 }
 document.onload = localImg.innerHTML = `<img class="robo" src="${coresImg[0]}" alt="Robotron"> <figcaption class="titulo">ROBOTRON <br>2000</figcaption>`
 
-valorIMG = 1
+var valorIMG = 1
 botaoCor.onclick = function(){
     if(valorIMG < quantidadeDeCor){
 localImg.innerHTML = `<img class="robo" src="${coresImg[valorIMG]}" alt="Robotron"> <figcaption class="titulo">ROBOTRON <br>2000</figcaption>`
@@ -21,15 +22,24 @@ valorIMG += 1
 }
 //atribuicao de valores aos botoes de somar e diminuir
 
-const controleContador = document.querySelectorAll('.controle-contador')
-const controleAjuste = Array.from(document.querySelectorAll('.controle-ajuste'))
-
+const controleAjuste = Array.from(document.querySelectorAll('[data-controle]'))
+const estatisticas = Array.from(document.querySelectorAll('[data-estatistica]'))
 controleAjuste.forEach(controle => controle.onclick = function(){
-    if(controleAjuste[controleAjuste.indexOf(controle)].textContent === '+'){
-        controleContador[parseInt(controleAjuste.indexOf(controle) /2)].value = Number(controleContador[parseInt(controleAjuste.indexOf(controle) /2)].value) + 1
-    }
-    else if(controleContador[parseInt(controleAjuste.indexOf(controle) /2)].value > 0){
-        controleContador[parseInt(controleAjuste.indexOf(controle) /2)].value = Number(controleContador[parseInt(controleAjuste.indexOf(controle) /2)].value) - 1
+    if(controleAjuste[controleAjuste.indexOf(controle)].dataset.controle === '+'){
+        controle.parentNode.querySelector('.controle-contador').value = parseInt(controle.parentNode.querySelector('.controle-contador').value) +1
+                estatisticas[0].innerText = parseInt(estatisticas[0].innerText) + pecas[controle.parentNode.dataset.peca].forca
+                estatisticas[1].innerText = parseInt(estatisticas[1].innerText) + pecas[controle.parentNode.dataset.peca].poder
+                estatisticas[2].innerText = parseInt(estatisticas[2].innerText) + pecas[controle.parentNode.dataset.peca].energia
+                estatisticas[3].innerText = parseInt(estatisticas[3].innerText) + pecas[controle.parentNode.dataset.peca].velocidade
+              } 
+    else if(controle.parentNode.querySelector('.controle-contador').value > 0 && controleAjuste[controleAjuste.indexOf(controle)].dataset.controle === '-'){
+        controle.parentNode.querySelector('.controle-contador').value = parseInt(controle.parentNode.querySelector('.controle-contador').value) -1
+                estatisticas[0].innerText = parseInt(estatisticas[0].innerText) - pecas[controle.parentNode.dataset.peca].forca
+                estatisticas[1].innerText = parseInt(estatisticas[1].innerText) - pecas[controle.parentNode.dataset.peca].poder
+                estatisticas[2].innerText = parseInt(estatisticas[2].innerText) - pecas[controle.parentNode.dataset.peca].energia
+                estatisticas[3].innerText = parseInt(estatisticas[3].innerText) - pecas[controle.parentNode.dataset.peca].velocidade
+    } else {
+        console.log('faltando dataset + ou -, ou valor negativo invalido')
     }
 })
 
